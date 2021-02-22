a029c1c0e3adc7ee1a50431e55b4008c3d47fd08
isrt09 <mazedur@gmail.com>
2021-02-23 02:17:01 +0600

    React JS : Class Component

---
+++ Class Component/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Laravel from './components/Laravel'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">                    
+          <Laravel></Laravel>          
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ Class Component/components/Laravel.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Laravel extends Component{
+    render(){
+        return <h1>LARAVEL is PHP Framework ....</h1>
+    }
+}
+
+export default Laravel
