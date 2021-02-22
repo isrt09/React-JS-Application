082bc27ede34282a6959c42bf0d1ce58237e6e16
isrt09 <mazedur@gmail.com>
2021-02-23 03:05:12 +0600

    React JS : Props

---
+++ Props/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Propper from './components/Propper'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">          
+          <Propper name="Maaa...."></Propper>
+          <Propper name="Papa...."></Propper>            
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
---
+++ Props/components/Propper.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class Propper extends Component{
+    render(){        
+        
+        return <h1>I love {this.props.name} very much</h1>        
+    }
+}
+
+export default Propper
